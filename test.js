f3d774d6b7709d877640677469db941f9b667a0b
Vandana <vandana@cloudlendinginc.com>
2017-02-28 22:46:41 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,25 +1,19 @@
 
 var Blocks = require('/home/vanrao/scratch-vm/LEAPER-master/src/engine/blocks');
+//var csv = require('ya-csv');
 var http = require('http');
 var url=require('url');
 var fs=require('fs');
+var prevcoor="";
 var server=http.createServer(function (req, res){
 
     console.log('Request received');
-    res.setHeader('Connection','close');
-    res.writeHead(200, { 
-        'Content-Type': 'text/plain',
-        'Access-Control-Allow-Origin': '*' // implementation of CORS
-    });
-    var data='';
-    req.on('data', function (chunk) {
-        data+=chunk.toString();
-    });
-    
+    console.log(prevcoor);
     var url_parts = url.parse(req.url, true);
     var query = url_parts.query;
     //console.log(query);             //query is a json object
     var arr=query['jsonData[]'];
+    var date=Date.now();
     var timestamp=arr[0];             // string
     var blockData=arr[1];             //string
 
@@ -33,30 +27,43 @@ var server=http.createServer(function (req, res){
     //Calling blocks.js functions to retrieve all the properties of a block
     var blockObj = new Blocks();
     
-
-    console.log(blockIDs);
+    var dataArray;
+    //console.log(blockIDs);
     for (var i = 0; i < blockIDs.length; i++) {
       //To retrieve,first create the block with that received data
       blockObj.createBlock(jsonObj['_blocks'][blockIDs[i]]);
       
       //use all the functions defined in blocks.js
-      console.log(blockObj.getBlock(blockIDs[i]));
-      console.log(blockObj.getNextBlock(blockIDs[i]));
-      console.log(blockObj.getBranch(blockIDs[i]));
-      console.log(blockObj.getOpcode(blockIDs[i]));
-      console.log(blockObj.getFields(blockIDs[i]));
-      console.log(blockObj.getInputs(blockIDs[i]));
+      var id=blockObj.getBlock(blockIDs[i])['id'];
+      var opcode=blockObj.getBlock(blockIDs[i])['opcode'];
+      var inputs=blockObj.getBlock(blockIDs[i])['inputs'];
+      var fields=blockObj.getBlock(blockIDs[i])['fields'];
+      var next=blockObj.getBlock(blockIDs[i])['next'];
+      var toplevel=blockObj.getBlock(blockIDs[i])['toplevel'];
+      var parent=blockObj.getBlock(blockIDs[i])['parent'];
+      var shadow=blockObj.getBlock(blockIDs[i])['shadow'];
+      var X=blockObj.getBlock(blockIDs[i])['x'];
+      var Y=blockObj.getBlock(blockIDs[i])['y'];
+
 
       // TODO : retrieve all these params and write it into a csv file
+      var onlyData=id+" "+opcode+" "+JSON.stringify(inputs)+" "+JSON.stringify(fields)+" "+next+" "+toplevel+" "+parent+" "+shadow;
+      var coordinates=X+" "+Y;
+      
+      
+        dataArray=date+" "+onlyData;
 
-    };
+      fs.appendFileSync('./InteractionData.csv',dataArray+"\n");
 
-    fs.appendFile('./input.txt',JSON.stringify(query)+"\n",function(err) {
+    
+
+    /*fs.appendFile('./input.txt',JSON.stringify(query)+"\n",function(err) {
     if (err)
     {
       return console.error(err);
     }
-  });
+  });*/
+}
 }).listen(8080, 'localhost');
 console.log('Server running at http://localhost');
 
