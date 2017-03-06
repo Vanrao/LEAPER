c8164bdf846b04b652842f048014ae93d6c398bd
Vandana <vandana@cloudlendinginc.com>
2017-03-06 13:04:01 +0530

    KMeans clustering on dataset

---
+++ datasetClustering.py
@@ -0,0 +1,20 @@
+from sklearn.feature_extraction.text import Tfidfvectorizer
+from sklearn.cluster import KMeans
+import numpy as np
+
+# TODO : Read data from the csv
+documents=[]
+
+#Remove all stopwords since all characters are taken
+vectorizer=Tfidfvectorizer(stop_words=None)
+Train_X=vectorizer.fit_transform(documents)
+
+num_Of_clusters=3
+model= KMeans(n_clusters=num_Of_clusters,init='k_means++',max_iter=100,n_init=1)
+model.fit(Train_X)
+
+labels=model.labels_
+order_centroids=model.cluster_centers_.argsort()[:, ::-1]
+
+#Predict the test label for new data.
+testLabels=model.predict(newTestData)
