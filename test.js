1528ce60e045cdf877cf75a167c963eb5f2010e8
Vandana <vandana@cloudlendinginc.com>
2017-03-24 21:25:48 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,31 +1,39 @@
-
 var Blocks = require('/home/vanrao/scratch-vm/LEAPER-master/src/engine/blocks');
-//var csv = require('ya-csv');
 var http = require('http');
 var url=require('url');
 var fs=require('fs');
 var prevcoor="";
+/*
+var milliseconds=1488359809244;
+var offset=10;
+var d = new Date(milliseconds+(3600000*offset));
+console.log(d.toUTCString());
+*/
 var server=http.createServer(function (req, res){
-
+    
     console.log('Request received');
     console.log(prevcoor);
     var url_parts = url.parse(req.url, true);
     var query = url_parts.query;
     //console.log(query);             //query is a json object
     var arr=query['jsonData[]'];
+    //var arr=query[['jsonData']];
     var date=Date.now();
     var timestamp=arr[0];             // string
     var blockData=arr[1];             //string
 
     //To retrieve the json data,first convert the string to JSON
     var jsonObj=JSON.parse(blockData);
-    
+    var jsonArr=[];
+
     //Get all the block IDs   
     var blockIDs=Object.keys(jsonObj['_blocks']);
     //var val=jsonObj['_scripts'];
     
     //Calling blocks.js functions to retrieve all the properties of a block
     var blockObj = new Blocks();
+    
+
     
     var dataArray;
     //console.log(blockIDs);
@@ -55,7 +63,7 @@ var server=http.createServer(function (req, res){
 
       fs.appendFileSync('./InteractionData.csv',dataArray+"\n");
 
-    
+
 
     /*fs.appendFile('./input.txt',JSON.stringify(query)+"\n",function(err) {
     if (err)
