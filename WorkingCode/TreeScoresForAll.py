7e48208241911219b7aab0a46e1bf5e81db8dcf8
Vandana <vandana@cloudlendinginc.com>
2017-04-18 16:36:02 +0530

    Update TreeScoresForAll.py

--- WorkingCode/TreeScoresForAll.py
+++ WorkingCode/TreeScoresForAll.py
@@ -1,19 +1,16 @@
 import os
 import getTreeEditdistScore
 
-i=1
-#csvdata = '/home/vanrao/scratch-vm/LEAPER-master/WorkingLeaper/'
 csvdata = "/home/vanrao/Trialdata/Level_1_Scores/"
 level2file = '/home/vanrao/Trialdata/Level_2_Scores/'
 
 filePath = "/home/vanrao/Trialdata/FinalOpcodes/"
 files = os.listdir("/home/vanrao/Trialdata/FinalOpcodes/")
 for i in range(len(files)):
-	newf = files[i].replace(".txt", "")
-	print csvdata+newf+".csv"
-	print level2file+newf+".csv"
-	if(i==6):
-		getTreeEditdistScore.createCsvs(csvdata+newf+".csv", level2file+newf+".csv", filePath+files[i])
-		break
-	#i=i+1
+	formattedName = files[i].replace(".txt", "")
+	print csvdata+formattedName+".csv"
+	print level2file+formattedName+".csv"
+	getTreeEditdistScore.createCsvs(csvdata+formattedName+".csv", level2file+formattedName+".csv", filePath+files[i])
+
+print "Done writing into files"
 
