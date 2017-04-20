ce5be6bd1198dae6fec49acb48245c163ce88e63
Vandana <vandana@cloudlendinginc.com>
2017-04-20 10:13:38 +0530

    Add files via upload

--- WorkingCode/TreeScoresForAll.py
+++ WorkingCode/TreeScoresForAll.py
@@ -1,16 +1,26 @@
 import os
 import getTreeEditdistScore
+import getTreeEditDistanceMin
 
+
+#csvdata = '/home/vanrao/scratch-vm/LEAPER-master/WorkingLeaper/'
 csvdata = "/home/vanrao/Trialdata/Level_1_Scores/"
 level2file = '/home/vanrao/Trialdata/Level_2_Scores/'
 
 filePath = "/home/vanrao/Trialdata/FinalOpcodes/"
 files = os.listdir("/home/vanrao/Trialdata/FinalOpcodes/")
 for i in range(len(files)):
-	formattedName = files[i].replace(".txt", "")
-	print csvdata+formattedName+".csv"
-	print level2file+formattedName+".csv"
-	getTreeEditdistScore.createCsvs(csvdata+formattedName+".csv", level2file+formattedName+".csv", filePath+files[i])
-
-print "Done writing into files"
+	getTreeEditdistScore.count = 0
+	getTreeEditdistScore.startTime = 0
+	getTreeEditdistScore.tree = ''
+	getTreeEditdistScore.treeList = []
+	getTreeEditdistScore.level1List = []
+	#if(files[i] == 'Tanay.txt'):
+	newf = files[i].replace(".txt", "")
+	print csvdata+newf+".csv"
+	print level2file+newf+".csv"
+	getTreeEditdistScore.createCsvs(csvdata+newf+".csv", level2file+newf+".csv", filePath+files[i])
+	#getTreeEditDistanceMin.createCsvs(csvdata+newf+"Min"+".csv", level2file+newf+"Min"+".csv", filePath+files[i])
+	#break
+	#i=i+1
 
--- WorkingCode/getTreeEditDistanceMin.py
+++ WorkingCode/getTreeEditDistanceMin.py
@@ -1,11 +1,10 @@
 import os
 import csv
-
-
+import datetime
+# can only be written into files
 #global tree
 tree = ''
 treeList = []
-level1List = []
 solutionsListForLevel1 = ["{root{whenbackdropswitchesto[level2]{hide{goto-195143}}}{whengreenflagclicked{goto-195203{show{pointindirection180{movesteps350{pointindirection90{movesteps450{Forever{if<touching[Stop]>then{switchbackdropto[level2]}}}}}}}}}}}", 
 "{root{whenbackdropswitchesto[level2]{hide{goto-195143}}}{whengreenflagclicked{goto-195203{show{pointindirection180{movesteps360{turnleft90{movesteps360{movesteps10{Forever{if<touching[Stop]>then{switchbackdropto[level2]}}}}}}}}}}}}","{root{whenbackdropswitchesto[level2]{hide{goto-195143}}}{whengreenflagclicked{goto-195203{show{pointindirection180{movesteps10{movesteps360{turnleft90{movesteps370{Forever{if<touching[Stop]>then{switchbackdropto[level2]}}}}}}}}}}}}","{root{whenbackdropswitchesto[level2]{hide{goto-195143}}}{whengreenflagclicked{goto-195203{show{pointindirection180{movesteps350{pointindirection90{movesteps390{Forever{if<touching[Stop]>then{switchbackdropto[level2]}}}}}}}}}}}","{root{whenbackdropswitchesto[level2]{hide{goto-195143}}}{whengreenflagclicked{goto-195203{show{pointindirection180{moveX380{movesteps350{Forever{if<touching[Stop]>then{switchbackdropto[level2]}}}}}}}}}}","{root{whenbackdropswitchesto[level2]{hide{goto-195143}}}{whengreenflagclicked{goto-195203{show{pointindirection180{movesteps100{movesteps100{movesteps100{movesteps50{moveX100{moveX100{moveX100{moveX50{moveX10{moveX10{Forever{if<touching[Stop]>then{switchbackdropto[level2]}}}}}}}}}}}}}}}}}}",
 "{root{whenbackdropswitchesto[level2]{hide{goto-195143}}}{whengreenflagclicked{goto-195203{show{pointindirection180{movesteps100{movesteps100{movesteps100{movesteps50{moveX100{moveX100{moveX100{moveX50{moveX10{moveX10{Forever{if<touching[Stop]>then{switchbackdropto[level2]}}}}}}}}}}}}}}}}}}","{root{whenbackdropswitchesto[level2]{hide{goto-195143}}}{whengreenflagclicked{goto-195203{show{pointindirection180{movesteps350{pointindirection90{movesteps420{Forever{if<touching[Stop]>then{switchbackdropto[level2]}}}}}}}}}}}",
@@ -16,9 +15,7 @@ solutionsListForLevel1 = ["{root{whenbackdropswitchesto[level2]{hide{goto-195143
 "{root{whenbackdropswitchesto[level2]{hide{goto-195143}}}{whengreenflagclicked{goto-195203{show{pointindirection180{movesteps70{movesteps250{movesteps60{pointindirection90{movesteps100{movesteps100{movesteps100{movesteps49{movesteps20{movesteps30{Forever{if<touching[Stop]>then{switchbackdropto[level2]}}}}}}}}}}}}}}}}}}"
 ]
 solutionsListForLevel2 = []
-startTime = 0
-count = 0
-#print len(solutionsListForLevel1)
+print len(solutionsListForLevel1)
 def createCsvs(studentCsv1, studentCsv2, studentInputFile):
 	studentScores = studentCsv1
 	studLevel2 = studentCsv2
@@ -38,25 +35,13 @@ def createCsvs(studentCsv1, studentCsv2, studentInputFile):
 	#for solution 1
 	ff = open(studentScores, 'w')
 	level2file = open(studLevel2, 'w')
-	
 	def csvwrite(timestamp, score):
 		csvwriter = csv.writer(ff, delimiter=',')
-		global count
-		global startTime
-		if(count == 0):
-			startTime = timestamp
-			count = 1
-			#print timestamp
-			percentageTime = (int(timestamp)-int(startTime)) / 1000 ;
-			#print percentageTime
-			'''csvwriter.writerow([timestamp, score[0], score[1], score[2], score[3], score[4], score[5], score[6], score[7], score[8], score[9], score[10], score[11], score[12], score[13], score[14], score[15], score[16], score[17], score[18], score[19], score[20], score[21], score[22], score[23],score[24], score[25], score[26], score[27]])'''
-			csvwriter.writerow([percentageTime,score])
-		else:
-			#print timestamp
-			percentageTime = (int(timestamp)-int(startTime)) / 1000 ;
-			#print percentageTime
-			'''csvwriter.writerow([timestamp, score[0], score[1], score[2], score[3], score[4], score[5], score[6], score[7], score[8], score[9], score[10], score[11], score[12], score[13], score[14], score[15], score[16], score[17], score[18], score[19], score[20], score[21], score[22], score[23],score[24], score[25], score[26], score[27]])'''
-			csvwriter.writerow([percentageTime,score])
+		time = datetime.datetime.fromtimestamp(float(timestamp)/1000.0).strftime('%H:%M:%S')
+		'''csvwriter.writerow([timestamp, score[0], score[1], score[2], score[3], score[4], score[5], score[6], score[7], score[8], score[9], score[10], score[11], score[12], score[13], score[14], score[15], score[16], score[17], score[18], score[19], score[20], score[21], score[22], score[23],score[24], score[25], score[26], score[27]])'''
+		#print score
+		#print min(score)
+		csvwriter.writerow([time,min(score)])
 	def csvwrite2(timestamp, score):
 		csvwriter = csv.writer(level2file, delimiter=',')
 		csvwriter.writerow([timestamp,  score[0]])
@@ -72,9 +57,6 @@ def createCsvs(studentCsv1, studentCsv2, studentInputFile):
 
 
 	def calcScore(studentSoln, listOfCorrectSolns):
-		#print studentSoln
-		#print "\n"
-		#print listOfCorrectSolns
 		scoresOrder = []
 		studentSoln = studentSoln.replace("<","")
 		studentSoln = studentSoln.replace(">","")
@@ -84,25 +66,11 @@ def createCsvs(studentCsv1, studentCsv2, studentInputFile):
 			eachSoln = eachSoln.replace("<","")
 			eachSoln = eachSoln.replace(">","")
 			eachSoln = eachSoln.replace("[","")
+			eachSoln = eachSoln.replace("]","")
 			score = os.popen("java -jar RTED_v1.1.jar -t"+" "+studentSoln+" "+eachSoln, "r")
 			#print score.read()
 			scoresOrder.append(float(score.read().rstrip()))
-		#print scoresOrder
-		#print min(scoresOrder)
 		return scoresOrder
-	def calcScoreSingle(studentSoln, correctSoln):
-		studentSoln = studentSoln.replace("<","")
-		studentSoln = studentSoln.replace(">","")
-		studentSoln = studentSoln.replace("[","")
-		studentSoln = studentSoln.replace("]","")
-		correctSoln = correctSoln.replace("<","")
-		correctSoln = correctSoln.replace(">","")
-		correctSoln = correctSoln.replace("[","")
-		correctSoln = correctSoln.replace("]","")
-		score = os.popen("java -jar RTED_v1.1.jar -t"+" "+studentSoln+" "+correctSoln, "r")
-		#print score.read()
-		return score.read().rstrip()
-		
 
 	with open(fileName,'r') as f:
 		data = f.read().splitlines()
@@ -117,54 +85,36 @@ def createCsvs(studentCsv1, studentCsv2, studentInputFile):
 		else:
 			treeList.append(tree)
 			tree = ''
-		#print "DATA "+tree
-		#print treeList
-		
+
 	#print treeList
-	
 	for item in treeList:
 		treeData = item.split(',')
 		if(len(treeData) == 3):
 			levelNum = treeData[0]
 			timestamp = treeData[1]
 			treeStructure = treeData[2]
-			#print "Level1 "+str(timestamp)
+			#with open(tempFile, 'w') as temp:
+			#	temp.write(treeStructure)
 			if(levelNum == 'Level 1'):
-				level1List.append(timestamp+","+treeStructure)
+				#print 'Level1'
+				treeScores = calcScore(treeStructure, solutionsListForLevel1)
+				#print timestamp+":"+str(treeScore)
+				csvwrite(timestamp, treeScores)
+				#open(tempFile, 'w').close()
+				#print "\n"
+			elif(levelNum == 'Level2'):
+				pass
+				'''
+				treeScores = calcScore(treeStructure,solutionsListForLevel2)
+				#print timestamp+":"+str(treeScore)
+				csvwrite2(timestamp, treeScores)
+				open(tempFile, 'w').close()
+				#print "\n"'''
 			
-	#print level1List[len(level1List)-1].split(",")[0]
-	init_scores = calcScore(level1List[len(level1List)-1].split(",")[1], solutionsListForLevel1)
-	#print min(init_scores)	
-	leastScoreIndex = init_scores.index(min(init_scores))
-	#print min(init_scores)
-	for item in level1List:
-		treeData = item.split(',')
-		timestamp = treeData[0]
-		treeStructure = treeData[1]
-		#with open(tempFile, 'w') as temp:
-		#temp.write(treeStructure)
-		#print 'Level1'
-		#treeScores1 = calcScore(treeStructure, solutionsListForLevel1)
-		treeScores = calcScoreSingle(treeStructure, solutionsListForLevel1[leastScoreIndex])
-		
-		#print timestamp+":"+str(treeScore)
-		#print "Initial "+str(timestamp)
-		csvwrite(timestamp, treeScores)
-		#open(tempFile, 'w').close()
-		#print "\n"
-		'''elif(levelNum == 'Level2'):
-			pass
 			
-			treeScores = calcScore(treeStructure,solutionsListForLevel2)
-			#print timestamp+":"+str(treeScore)
-			csvwrite2(timestamp, treeScores)
-			open(tempFile, 'w').close()
-			#print "\n"
-		
-		
 		elif(len(treeData)==2):
 			pass
-			timestamp = treeData[0]
+			'''timestamp = treeData[0]
 			treeStructure = treeData[1]
 			#with open(tempFile, 'w') as temp:
 			#	temp.write(treeStructure)
@@ -173,6 +123,7 @@ def createCsvs(studentCsv1, studentCsv2, studentInputFile):
 			csvwrite(timestamp, treeScores)
 			open(tempFile, 'w').close()
 			#print "\n"'''
+		
 	
 
 
